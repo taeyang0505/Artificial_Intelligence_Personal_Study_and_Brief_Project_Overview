b672480458d7a6b4f9e13d066e73dd8a4e1b3bad
TaeYang Hong <121171334+taeyang0505@users.noreply.github.com>
2024-01-08 02:59:18 +0900

    Create script2.py

---
+++ script2.py
@@ -0,0 +1,3 @@
+mile_time = 'continuous'  // a random variable that represents the time it takes a randomly selected person to run a mile
+
+candy = 'discrete'  // a random variable that represents the individual pieces of candy a randomly selected child receives on a candy-giving holiday
