a27a619d4747f3fcde9c7c9f5415fa567b15b7c5
TaeYang Hong <121171334+taeyang0505@users.noreply.github.com>
2024-01-08 00:25:16 +0900

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,3 +1,3 @@
-events_1 = "insert answer here"
-events_2 = "insert answer here"
-events_3 = "insert answer here"
+events_1 = "insert answer here"  // Events_1 is whether events A and B are independent or dependent on each other when 52 cards are drawn, event A is the first time to draw an ACE and event B is the second time to draw an ace
+events_2 = "insert answer here"  // When rolling the six-sided dice twice, is the case where the first 3 comes out and the case where the second 3 comes out independent or dependent on each other
+events_3 = "insert answer here"  // Is it independent or dependent when the first 5 tosses the coin 10 times and the second 5 all the way tosses the coin
