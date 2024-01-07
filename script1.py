b2eff8bffbd30e61299798b38328eb64c8597a58
TaeYang Hong <121171334+taeyang0505@users.noreply.github.com>
2024-01-08 02:55:45 +0900

    Create script1.py

---
+++ script1.py
@@ -0,0 +1,24 @@
+// random.choice(a, size = size, replace = True/False) from the numpy
+// a is a list or other object that has values we are sampling from
+// size is a number that represents how many values to choose
+// replace can be equal to True or False, and determines whether we keep a value in a after drawing it (replace = True) or remove it from the pool (replace = False)
+
+
+import numpy as np
+
+# create 6 sided "die"
+die_6 = range(1, 7)
+
+# set number of rolls
+num_rolls = 10
+
+# roll the "die" the set amount of times
+results_1 = np.random.choice(die_6, size = num_rolls, replace = True)
+print(results_1)
+
+# create 12-sided "die"
+die_12 = range(1, 13)
+
+# roll the 12-sided "die" 10 times
+results_2 = np.random.choice(die_12, size = num_rolls, replace = True)
+
