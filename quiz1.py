7f0b4b1d99289d30905e366a4387dd5387748d88
TaeYang Hong <121171334+taeyang0505@users.noreply.github.com>
2024-01-08 00:33:55 +0900

    Create quiz1.py

---
+++ quiz1.py
@@ -0,0 +1,3 @@
+events_1 = "mutually exclusive"  // Is it mutually exclusive to take out green beads from a bag containing three green beads and two blue beads
+events_2 = "not mutually exclusive"  // When you roll the dice once, you get an odd number and a number greater than four
+events_3 = "mutually exclusive"  // When you roll the dice once, you get an even number and a number less than two
