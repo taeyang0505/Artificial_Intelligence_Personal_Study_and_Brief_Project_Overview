a6b56881b3e514b88293dd52c13a629fbc8ccc02
TaeYang Hong <121171334+taeyang0505@users.noreply.github.com>
2024-01-08 16:13:04 +0900

    Create script15.py

---
+++ script15.py
@@ -0,0 +1,17 @@
+import scipy.stats as stats
+
+## Practice Question 1
+calls = 1 - stats.poisson.cdf(12, 9)
+print(calls)
+
+## Practice Question 2
+false_backup = stats.poisson.cdf(12, 9) - stats.poisson.cdf(9, 9)
+print(false_backup)
+
+## Practice Question 3
+expected_serves = 80 * 0.62
+print(expected_serves)
+
+## Practice Question 4
+variance_serves = 80 * 0.62 * 0.38
+print(variance_serves)
