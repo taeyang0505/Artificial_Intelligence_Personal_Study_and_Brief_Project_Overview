c4c68f2db30195aa8e33a7e1947f725c8f11b100
TaeYang Hong <121171334+taeyang0505@users.noreply.github.com>
2024-01-08 15:46:09 +0900

    Create script13.py

---
+++ script13.py
@@ -0,0 +1,9 @@
+// Variance(# of Heads) = Var(X) = n × p × (1−p)
+
+## Checkpoint 1
+variance_baskets = 20 * 0.85 * 0.15
+print(variance_baskets)
+
+## Checkpoint 2
+variance_late = 180 * 0.98 * 0.02
+print(variance_late)
