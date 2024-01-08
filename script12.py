428fb8c100541aed9cbb3e8fe88b85fd1f466193
TaeYang Hong <121171334+taeyang0505@users.noreply.github.com>
2024-01-08 15:39:37 +0900

    Create script12.py

---
+++ script12.py
@@ -0,0 +1,10 @@
+// the equation for the expected value of the binomial distribution
+// Expected(# of Heads) = E(X) = n × p
+
+## Checkpoint 1
+expected_baskets = 20 * 0.85 
+print(expected_baskets)
+
+## Checkpoint 2
+expected_late = 180 * 0.02
+print(expected_late) 
