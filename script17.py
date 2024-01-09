caf293f2164c3a8ea6417ff6baa59766fc21f477
TaeYang Hong <121171334+taeyang0505@users.noreply.github.com>
2024-01-10 00:22:18 +0900

    Create script17.py

---
+++ script17.py
@@ -0,0 +1,34 @@
+# np.random.choice() generates a sample of some size from a given array
+# np.mean() and round() that we used to calculate the population mean
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import codecademylib3
+
+population = pd.read_csv("salmon_population.csv")
+population = np.array(population.Salmon_Weight)
+pop_mean = round(np.mean(population),3)
+
+## Plotting the Population Distribution
+sns.histplot(population, stat='density')
+plt.axvline(pop_mean,color='r',linestyle='dashed')
+plt.title(f"Population Mean: {pop_mean}")
+plt.xlabel("Weight (lbs)")
+plt.show()
+plt.clf() # close this plot
+
+samp_size = 30
+# Generate our random sample below
+sample = np.random.choice(np.array(population), samp_size, replace = False)
+
+### Define sample mean below
+sample_mean = round(np.mean(sample), 3)
+
+### Uncomment the lines below to plot the sample data:
+sns.histplot(sample, stat='density')
+plt.axvline(sample_mean,color='r',linestyle='dashed')
+plt.title(F"Sample Mean: {sample_mean}")
+plt.xlabel("Weight (lbs)")
+plt.show()
